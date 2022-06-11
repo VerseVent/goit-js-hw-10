9da6e1909757841a561fceae9d098b6b2fe7aed4
VerseVent <mike.82pronka.27a@gmail.com>
2022-06-11 13:52:22 +0200

    fix fetch

--- src/js/APIservice/fetchCountries.js
+++ src/js/APIservice/fetchCountries.js
@@ -1,4 +1,4 @@
-export default fetchCountries = function (searchValue) {
-  let searchUrl = `https://restcountries.com/v2/name/${searchValue}?fields=name,capital,flag,languages,population`;
+export default function fetchCountries(searchValue) {
+  let searchUrl = `https://restcountries.com/v2/name/${searchValue}?fields=name,capital,currencies,flag,languages,population`;
   return fetch(searchUrl).then(response => response.json());
-};
+}
