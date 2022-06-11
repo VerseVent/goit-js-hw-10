d71bbd745bd46f5d7edd9f4aadec7d2f6742ee04
VerseVent <mike.82pronka.27a@gmail.com>
2022-06-11 13:13:10 +0200

    complete task 10

---
+++ src/js/APIservice/fetchCountries.js
@@ -0,0 +1,4 @@
+export default fetchCountries = function (searchValue) {
+  let searchUrl = `https://restcountries.com/v2/name/${searchValue}?fields=name,capital,flag,languages,population`;
+  return fetch(searchUrl).then(response => response.json());
+};
